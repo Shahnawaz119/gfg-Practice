55760f64e273fe1ab688d36962a4f6a1c583f065
Shahnawaz <150775839+Shahnawaz119@users.noreply.github.com>
2025-08-13 18:31:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nth Catalan Number/nth-catalan-number.cpp
+++ Difficulty: Medium/Nth Catalan Number/nth-catalan-number.cpp
@@ -1,22 +1,16 @@
 class Solution {
   public:
     // Function to find the nth catalan number.
-    int solve(int n,vector<int> &dp){
-        if(n==0 || n==1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int ans=0;
-        for(int i=0; i<n; i++){
-            ans+=solve(i,dp)*solve(n-1-i,dp);
-        }
-        return dp[n]=ans;
-    }
     int findCatalan(int n) {
         // code here
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        vector<int> dp(n+1,0);
+        dp[0]=dp[1]=1;
+        for(int i=2; i<=n; i++){
+            for(int j=0; j<i; j++){
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+        
     }
 };
